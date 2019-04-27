ac987c97f8202429b711750393a23dec19bafca9
claytonjhigbee <higbee@unlv.nevada.edu>
2019-04-27 11:20:05 -0700

    Recent upload

---
+++ Arduino - Microcontroller Code/AngleCaptureTestV1/AngleCaptureTestV1.ino
@@ -0,0 +1,23 @@
+int testvariable = 1;
+int val = 0;
+
+
+void setup() {
+  Serial.begin(115200);
+}
+
+void loop() {
+  if(testvariable >= 135){
+    testvariable = 45;
+  }
+  else{
+    testvariable = testvariable + 1;
+  }
+  val = testvariable;
+Serial.println(val);
+delay(200);
+}
+
+void serialEvent(){
+  Serial.println("It worked!");
+  }
